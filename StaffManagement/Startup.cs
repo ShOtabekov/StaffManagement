2147a9d26dbc640385eeeb157cd21c70f6e754c2
ShOtabekov <sh.otabekov@mail.ru>
2022-07-31 14:48:04 +0300

    Working with MVC in the project Added new folder Controllers and added file HomeController

--- StaffManagement/Startup.cs
+++ StaffManagement/Startup.cs
@@ -23,22 +23,24 @@ namespace StaffManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            /*if (env.IsDevelopment())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-*/
 
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
             /*app.UseDefaultFiles();
             app.UseStaticFiles();*/
             //app.UseFileServer();
-            app.UseRouting();
+            /*app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
@@ -46,7 +48,7 @@ namespace StaffManagement
                     throw new  Exception("Xato ");
                     await context.Response.WriteAsync("Main page ");
                 });
-            });
+            });*/
         }
     }
 }
